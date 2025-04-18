12f82e3cbe13b1db5dd906beba36596577f33d10
Arctic Overclockers <arcticoverclockers@gmail.com>
2025-04-18 02:15:06 +0200

    Update totp.py

--- totp.py
+++ totp.py
@@ -1,3 +1,12 @@
 import pyotp
-totp = pyotp.TOTP("YOURSECRETTOTPKEY")
+import os
+
+# Fetch the TOTP secret key from environment variables
+totp_secret_key = os.getenv("TOTP_SECRET")
+
+if not totp_secret_key:
+    raise ValueError("TOTP_SECRET environment variable is not set!")
+
+# Initialize TOTP with the secret key
+totp = pyotp.TOTP(totp_secret_key)
 print(totp.now())  # Will match your phone!
